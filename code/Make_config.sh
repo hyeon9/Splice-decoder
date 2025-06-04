c82912f7fef3eadcf3b49f9d87e042c696f06faf
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-06-04 12:19:43 -0400

    Update Make_config.sh

--- code/Make_config.sh
+++ code/Make_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "To make right config file, you should give right answer for each question!"
-source /projects/anczukow-lab/kangh/miniforge-pypy3/bin/activate base
+source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate splice-decoder
+conda_path=`conda info --envs | grep '*' | awk '{print $NF}'`
 export PATH="`pwd`:$PATH"
-config_maker.py
+config_maker.py ${conda_path}

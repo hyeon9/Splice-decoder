66aa1372dbfeb3d6686a29de5906756e386ab5c9
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-06-16 10:16:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yml=$1
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda env create --name testenv -f ${yml} -k &&
-conda activate testenv &&
+conda env create --name splice-decoder -f ${yml} -k &&
+conda activate splice-decoder &&
 pip install -r requirements_pip.txt

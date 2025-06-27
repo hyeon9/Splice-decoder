c0ecc24badd70f4dbabe7032e8db2e51b4964317
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-06-27 13:21:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ unzip toy_data.zip
 
 ## Prepara data source
 unzip dat.zip
-cd /dat
-cd reference
+cd dat/
+cd reference/
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/GRCh38.primary_assembly.genome.fa.gz
-gunzip -c GRCm38.primary_assembly.genome.fa.gz > hg38.fa 
+gunzip -c GRCh38.primary_assembly.genome.fa.gz > hg38.fa 
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/GRCm38.primary_assembly.genome.fa.gz
 gunzip -c GRCm38.primary_assembly.genome.fa.gz > GRCm38.fa

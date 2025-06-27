839955d4b25585d128bdb424b698463bd931ea40
hyeon9 <sptka0000@gmail.com>
2025-06-27 13:09:57 -0400

    update

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ conda activate splice-decoder &&
 pip install -r requirements_pip.txt
 
 ## Prepara data source
-# unzip dat.zip
-# cd /dat
-# cd reference
-# wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/GRCh38.primary_assembly.genome.fa.gz
-# gunzip GRCh38.primary_assembly.genome.fa.gz
-# wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/GRCm38.primary_assembly.genome.fa.gz
-# gunzip GRCm38.primary_assembly.genome.fa.gz
+unzip dat.zip
+cd /dat
+cd reference
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/GRCh38.primary_assembly.genome.fa.gz
+gunzip -c GRCm38.primary_assembly.genome.fa.gz > hg38.fa 
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/GRCm38.primary_assembly.genome.fa.gz
+gunzip -c GRCm38.primary_assembly.genome.fa.gz > GRCm38.fa 

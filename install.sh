b1b8238a4e8792fe5b8855f17ae3124446972bcc
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-06-27 12:45:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,12 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env create --name splice-decoder -f ${yml} -k &&
 conda activate splice-decoder &&
 pip install -r requirements_pip.txt
+
+## Prepara data source
+# unzip dat.zip
+# cd /dat
+# cd reference
+# wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/GRCh38.primary_assembly.genome.fa.gz
+# gunzip GRCh38.primary_assembly.genome.fa.gz
+# wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/GRCm38.primary_assembly.genome.fa.gz
+# gunzip GRCm38.primary_assembly.genome.fa.gz

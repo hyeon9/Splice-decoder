10cd640c4bdfa47c00342034b1e9214898fcb22b
hyeon9 <sptka0000@gmail.com>
2025-06-30 11:24:55 -0400

    update

--- code/config_maker.py
+++ code/config_maker.py
@@ -14,19 +14,26 @@ config['Your_rMATS'] = questionary.text("Enter your rMATS output path").ask()+"/
 config['Your_GTF'] = questionary.text("Enter your GTF file that you used in rMATS with its full path").ask()
 
 ## Scoring
-config['get_score'] = questionary.text("Do you want to calculated the effect score? [yes/no]").ask()
-if config['get_score'] == "no" or config["get_score"] == "No":
+if output_prefix != "toy":
+	config['get_score'] = questionary.text("Do you want to calculated the effect score? [yes/no]").ask()
+	if config['get_score'] == "no" or config['get_score'] == "No":
+		config['Your_TPM'] = "N"
+		config['bam_list'] = "N"
+		config['tpm'] = "N"
+	else:
+		config['Your_TPM'] = questionary.text("Enter your TPM matrix with full path. If you don't have one type N").ask()
+		if config['Your_TPM'] == "N":	# SpliceDecoder will calculate TPM by using stringtie with the given bamfiles
+			config['bam_list'] = questionary.text("Enter your bamlist which should contains bamfile with their full path in each line").ask()
+			config['tpm'] = "Y"
+		else:	# SpliceDecoder will use the given TPM matrix
+			config['bam_list'] = "N"
+			config['tpm'] = "Y_own"
+
+else:	# For toy data set
+	config['get_score'] = "no"
 	config['Your_TPM'] = "N"
 	config['bam_list'] = "N"
 	config['tpm'] = "N"
-else:
-	config['Your_TPM'] = questionary.text("Enter your TPM matrix with full path. If you don't have one type N").ask()
-	if config['Your_TPM'] == "N":	# SpliceDecoder will calculate TPM by using stringtie with the given bamfiles
-		config['bam_list'] = questionary.text("Enter your bamlist which should contains bamfile with their full path in each line").ask()
-		config['tpm'] = "Y"
-	else:	# SpliceDecoder will use the given TPM matrix
-		config['bam_list'] = "N"
-		config['tpm'] = "Y_own
 
 config['species'] = questionary.text("Enter a species of your data").ask()
 config['seq_type'] = questionary.text("Enter a sequencing type of your data e.g., SR (short-read) or LR (long-read)").ask()
@@ -34,6 +41,7 @@ config['FDR'] = questionary.text("Enter a FDR cut off for your rMATS").ask()
 config['PSI'] = questionary.text("Enter a |dPSI| cut off for your rMATS").ask()
 config['njobs'] = questionary.text("Enter a number of cpu in splice-decoder job").ask()
 
+## Specify 3rd party tool path
 config['conda'] = sys.argv[1]
 config['cpat'] = os.path.join(config['conda'], "bin/cpat")
 config['bedtools'] = os.path.join(config['conda'], "bin/bedtools")

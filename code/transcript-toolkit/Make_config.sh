c2b76eb4dd2462941be75e998893395773376f36
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-07-31 14:56:17 -0400

    Create Make_config.sh

---
+++ code/transcript-toolkit/Make_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "To make right config file, you should give right answer for each question!"
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate TX-comp
+conda_path=`conda info --envs | grep '*' | awk '{print $NF}'`
+export PATH="`pwd`:$PATH"
+toy_key="${1:-data}"
+
+config_maker.py ${conda_path} ${toy_key}

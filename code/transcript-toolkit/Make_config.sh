370fb42233eeb80638e7d7a7aba284af71a91a74
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-07-31 14:56:35 -0400

    Update Make_config.sh

--- code/transcript-toolkit/Make_config.sh
+++ code/transcript-toolkit/Make_config.sh
@@ -2,7 +2,7 @@
 
 echo "To make right config file, you should give right answer for each question!"
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate TX-comp
+conda activate splice-decoder
 conda_path=`conda info --envs | grep '*' | awk '{print $NF}'`
 export PATH="`pwd`:$PATH"
 toy_key="${1:-data}"

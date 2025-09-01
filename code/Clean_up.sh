0000f3e17e46c501113edcd8672defd7d1be0152
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-09-01 17:37:58 -0400

    Create Clean_up.sh

---
+++ code/Clean_up.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+input=$1
+cd ${input}
+
+mkdir -p temp
+mv *bed temp/
+mv *gtf temp/
+mv *txt temp/
+mv tx_gene_dict temp/
+mv sim_bed/ temp/
+mv get_Fasta/ temp/
+mv cpat/ temp/
+mv tpm/ temp/
+mv rmat.csv temp/
+
+echo "All intermediate files were moved to ${input}temp"

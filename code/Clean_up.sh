ad1e1b341b1e6fb36f0f8444438932cf583c4858
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-09-02 09:54:28 -0400

    Update Clean_up.sh

--- code/Clean_up.sh
+++ code/Clean_up.sh
@@ -2,7 +2,11 @@
 input=$1
 cd ${input}
 
+## Make temp folder AND post_input folder
 mkdir -p temp
+mkdir -p post_input
+
+## Clean up working directory
 mv *bed temp/
 mv *gtf temp/
 mv *txt temp/
@@ -11,5 +15,8 @@ mv get_Fasta/ temp/
 mv cpat/ temp/
 mv tpm/ temp/
 mv rmat.csv temp/
+
+## Move some files to post_input
+mv ./temp/merged*bed post_input/
 
 echo "All intermediate files were moved to ${input}temp"

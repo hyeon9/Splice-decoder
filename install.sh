98423886852a156c19a241740925997563c17745
hyeon9 <51947181+hyeon9@users.noreply.github.com>
2025-09-11 11:03:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@ unzip dat.zip
 cd dat/
 cd reference/
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/GRCh38.primary_assembly.genome.fa.gz
-gunzip -c GRCh38.primary_assembly.genome.fa.gz > hg38.fa 
+gunzip -c GRCh38.primary_assembly.genome.fa.gz > hg38.fa
+samtools faidx hg38.fa
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/GRCm38.primary_assembly.genome.fa.gz
 gunzip -c GRCm38.primary_assembly.genome.fa.gz > GRCm38.fa
+samtools faidx GRCm38.fa
